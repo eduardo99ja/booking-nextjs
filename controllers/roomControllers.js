58973cd3cf3997d1a1063b9e2f92ab7c4bf854b1
Eduardo Apodaca <eduardo1ja99@gmail.com>
2022-02-05 18:05:41 -0600

    feat: Create new Room

--- controllers/roomControllers.js
+++ controllers/roomControllers.js
@@ -1,12 +1,28 @@
+import Room from '../models/room'
 
-
-const allRooms = (req,res)=>{
+const allRooms = (req, res) => {
   res.status(200).json({
     success: true,
-    message: 'All rooms'
+    message: 'All rooms',
   })
 }
 
-export {
-  allRooms
+// Create new room =>   /api/rooms
+
+const newRoom = async (req, res) => {
+  try {
+    const room = await Room.create(req.body)
+
+    res.status(200).json({
+      success: true,
+      room,
+    })
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error: error.message,
+    })
+  }
 }
+
+export { allRooms, newRoom }
--- pages/api/rooms/index.js
+++ pages/api/rooms/index.js
@@ -1,10 +1,12 @@
 import nc from 'next-connect'
 import dbConnect from '../../../config/dbConnect'
 
-import { allRooms } from '../../../controllers/roomControllers'
+import { allRooms, newRoom } from '../../../controllers/roomControllers'
 
 dbConnect()
 const handler = nc()
+
 handler.get(allRooms)
+handler.post(newRoom)
 
 export default handler
