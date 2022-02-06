ed4738a54afbed56ef34432b5c74263ddb669981
Eduardo Apodaca <eduardo1ja99@gmail.com>
2022-02-05 20:57:23 -0600

    feat: Error middleware

--- pages/api/rooms/index.js
+++ pages/api/rooms/index.js
@@ -2,9 +2,10 @@ import nc from 'next-connect'
 import dbConnect from '../../../config/dbConnect'
 
 import { allRooms, newRoom } from '../../../controllers/roomControllers'
+import onError from '../../../middlewares/errors'
 
 dbConnect()
-const handler = nc()
+const handler = nc({ onError })
 
 handler.get(allRooms)
 handler.post(newRoom)
