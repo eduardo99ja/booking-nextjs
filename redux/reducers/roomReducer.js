e3231c7dabae77515b5cf1619393edba163aa6f1
Eduardo Apodaca <eduardo1ja99@gmail.com>
2022-02-06 22:05:12 -0600

    feat: Room details reducer and actions

--- redux/reducers/roomReducer.js
+++ redux/reducers/roomReducer.js
@@ -1,6 +1,8 @@
 import {
   ALL_ROOMS_SUCCESS,
   ALL_ROOMS_FAIL,
+  ROOM_DETAILS_SUCCESS,
+  ROOM_DETAILS_FAIL,
   CLEAR_ERRORS,
 } from '../constants/roomConstants'
 
@@ -25,6 +27,30 @@ export const allRoomsReducer = (state = { rooms: [] }, action) => {
         ...state,
         error: null,
       }
+    default:
+      return state
+  }
+}
+
+// Room details reducer
+export const roomDetailsReducer = (state = { room: {} }, action) => {
+  switch (action.type) {
+    case ROOM_DETAILS_SUCCESS:
+      return {
+        room: action.payload,
+      }
+
+    case ROOM_DETAILS_FAIL:
+      return {
+        error: action.payload,
+      }
+
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null,
+      }
+
     default:
       return state
   }
