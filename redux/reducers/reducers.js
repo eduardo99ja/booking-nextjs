e618c59a12a9d12a9a4bf4bb4501300125df4484
Eduardo Apodaca <eduardo1ja99@gmail.com>
2022-02-06 19:01:53 -0600

    feat: All rooms reducer

--- redux/reducers/reducers.js
+++ redux/reducers/reducers.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux'
+import { allRoomsReducer } from './roomReducer'
 
 const reducer = combineReducers({
-
+  allRooms: allRoomsReducer,
 })
 
 export default reducer
