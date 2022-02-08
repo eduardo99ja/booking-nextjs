97c141ef3cfb6faf09df58d6a4f42c54b6343cd6
Eduardo Apodaca <eduardo1ja99@gmail.com>
2022-02-07 18:02:38 -0600

    feat: Pagination in the frontend

--- components/Home.js
+++ components/Home.js
@@ -1,39 +1,85 @@
 import React, { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 import { useDispatch, useSelector } from 'react-redux'
 import { toast } from 'react-toastify'
+import Pagination from 'react-js-pagination'
 import { clearErrors } from '../redux/actions/roomActions'
 
 import RoomItem from './room/RoomItem'
 
 const Home = () => {
   const dispatch = useDispatch()
+  const router = useRouter()
 
-  const { rooms, error } = useSelector(state => state.allRooms)
+  const { rooms, resPerPage, roomsCount, filteredRoomsCount, error } =
+    useSelector(state => state.allRooms)
 
   useEffect(() => {
     toast.error(error)
     dispatch(clearErrors())
   }, [dispatch, error])
 
-  return (
-    <section id='rooms' className='container mt-5'>
-      <h2 className='mb-3 ml-2 stays-heading'>Stays in New York</h2>
+  let { location, page = 1 } = router.query
+  page = Number(page)
 
-      <a href='#' className='ml-2 back-to-search'>
-        {' '}
-        <i className='fa fa-arrow-left'></i> Back to Search
-      </a>
-      <div className='row'>
-        {rooms && rooms.length === 0 ? (
-          <div className='alert alert-danger mt-5 w-100'>
-            <b>No Rooms.</b>
-          </div>
-        ) : (
-          rooms && rooms.map(room => <RoomItem key={room._id} room={room} />)
-        )}
-      </div>
-    </section>
+  const handlePagination = pageNumber => {
+    if (location) {
+      let url = window.location.search
+
+      url.includes('&page')
+        ? (url = url.replace(/(page=)[^\&]+/, '$1' + pageNumber))
+        : (url = url.concat(`&page=${pageNumber}`))
+
+      router.push(url)
+    } else {
+      router.push(`/?page=${pageNumber}`)
+      // window.location.href = `/?page=${pageNumber}`
+    }
+  }
+
+  let count = roomsCount
+  if (location) {
+    count = filteredRoomsCount
+  }
+
+  return (
+    <>
+      <section id='rooms' className='container mt-5'>
+        <h2 className='mb-3 ml-2 stays-heading'>Stays in New York</h2>
+
+        <a href='#' className='ml-2 back-to-search'>
+          {' '}
+          <i className='fa fa-arrow-left'></i> Back to Search
+        </a>
+        <div className='row'>
+          {rooms && rooms.length === 0 ? (
+            <div className='alert alert-danger mt-5 w-100'>
+              <b>No Rooms.</b>
+            </div>
+          ) : (
+            rooms && rooms.map(room => <RoomItem key={room._id} room={room} />)
+          )}
+        </div>
+      </section>
+
+      {resPerPage < count && (
+        <div className='d-flex justify-content-center mt-5'>
+          <Pagination
+            activePage={page}
+            itemsCountPerPage={resPerPage}
+            totalItemsCount={roomsCount}
+            onChange={handlePagination}
+            nextPageText={'Next'}
+            prevPageText={'Prev'}
+            firstPageText={'First'}
+            lastPageText={'Last'}
+            itemClass='page-item'
+            linkClass='page-link'
+          />
+        </div>
+      )}
+    </>
   )
 }
 
