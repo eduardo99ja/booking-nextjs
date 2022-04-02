f486249a173212d03765ed541316002cfd1960ce
Eduardo Apodaca <eduardo2ja99@gmail.com>
2022-04-01 22:43:40 -0600

    Adding cloudinary for images

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,10 +1,23 @@
 import User from '../models/user'
+import cloudinary from 'cloudinary'
 
 import ErrorHandler from '../utils/errorHandler'
 import catchAsyncErrors from '../middlewares/catchAsyncErrors'
 
+// Setting up cloudinary config
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+})
+
 // Register user   =>   /api/auth/register
 const registerUser = catchAsyncErrors(async (req, res) => {
+  const result = await cloudinary.v2.uploader.upload(req.body.avatar, {
+    folder: 'bookit/avatars',
+    width: '150',
+    crop: 'scale',
+  })
   const { name, email, password } = req.body
 
   const user = await User.create({
@@ -12,8 +25,8 @@ const registerUser = catchAsyncErrors(async (req, res) => {
     email,
     password,
     avatar: {
-      public_id: 'PUBLIC_ID',
-      url: 'URL',
+      public_id: result.public_id,
+      url: result.secure_url,
     },
   })
 
