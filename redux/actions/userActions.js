d7681f73b588fe497c8ed598453222ca893f7917
Eduardo Apodaca <eduardo2ja99@gmail.com>
2022-04-16 15:06:03 -0500

    feat: Load Logged In user

--- redux/actions/userActions.js
+++ redux/actions/userActions.js
@@ -4,6 +4,9 @@ import {
   REGISTER_USER_REQUEST,
   REGISTER_USER_SUCCESS,
   REGISTER_USER_FAIL,
+  LOAD_USER_REQUEST,
+  LOAD_USER_SUCCESS,
+  LOAD_USER_FAIL,
   CLEAR_ERRORS,
 } from '../constants/userConstants'
 
@@ -31,9 +34,28 @@ export const registerUser = userData => async dispatch => {
   }
 }
 
+// Loader user
+export const loadUser = () => async dispatch => {
+  try {
+    dispatch({ type: LOAD_USER_REQUEST })
+
+    const { data } = await axios.get('/api/me')
+    console.log("Se llamo 1 vez");
+
+    dispatch({
+      type: LOAD_USER_SUCCESS,
+      payload: data.user,
+    })
+  } catch (error) {
+    dispatch({
+      type: LOAD_USER_FAIL,
+      payload: error.response.data.message,
+    })
+  }
+}
 // Clear Errors
-export const clearErrors = () => async (dispatch) => {
+export const clearErrors = () => async dispatch => {
   dispatch({
-      type: CLEAR_ERRORS
+    type: CLEAR_ERRORS,
   })
 }
