8549fdb7dcce2f88553042b5a7f92b7689bb2565
Eduardo Apodaca <eduardo2ja99@gmail.com>
2022-04-16 14:11:25 -0500

    feat: Current User Profile

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -36,4 +36,14 @@ const registerUser = catchAsyncErrors(async (req, res) => {
   })
 })
 
-export { registerUser }
+// Cuurent user profile   =>   /api/me
+const currentUserProfile = catchAsyncErrors(async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+  res.status(200).json({
+    success: true,
+    user,
+  })
+})
+
+export { registerUser, currentUserProfile }
