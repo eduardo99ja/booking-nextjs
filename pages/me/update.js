8f31f899f9dc7a7220131877cf14cfd6f5173be1
Eduardo Apodaca <eduardo2ja99@gmail.com>
2022-04-17 15:38:29 -0500

    feat: Protect routes with getServerSideProps

---
+++ pages/me/update.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { getSession } from 'next-auth/client'
+
+
+
+const UpdateProfilePage = () => {
+    return (
+        <div>
+          <h1>User profile</h1>
+        </div>
+    )
+}
+
+export async function getServerSideProps(context) {
+
+    const session = await getSession({ req: context.req })
+
+    if (!session) {
+        return {
+            redirect: {
+                destination: '/login',
+                permanent: false
+            }
+        }
+    }
+
+    return {
+        props: { session }
+    }
+
+}
+
+export default UpdateProfilePage
